1758e59e78f7a8df6d6bc5c3b766cf0a72984525
landlordxu <xpchuanqi@163.com>
2016-12-27 16:10:36 +0800

    生成规则修改
    生成规则修改

--- creditCode.js
+++ creditCode.js
@@ -1,43 +1,323 @@
 //  统一社会信用代码 
-function isCreditCode(creditCode){   
-	toastr.options.positionClass = 'toast-center-center';
+function isCreditCode(creditCode){
+  toastr.options.positionClass = 'toast-center-center';
   toastr.options.timeOut = 1000;
-	if (creditCode.length != 18) {
-	 	toastr.warning('长度不等于18位!');       
-		return false;       
-	} 
-  var reg = /^([0-9A-Z]){18}$/;
-  if (!reg.test(creditCode)) {
-    toastr.warning('错误的统一社会信用代码!'); 
-    return false;
-  } 
-  var str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var wi =[1,3,9,27,19,26,16,17,20,29,25,13,8,24,10,30,28];
-	var sum = 0;
-    for (var i = 0; i < 17; i++) {
-      sum += str.indexOf(creditCode.charAt(i)) * wi[i];
+
+  creditCode = creditCode.toUpperCase();
+  if(creditCode.length!=18){
+    toastr.warning('信用代码证必须为18位!'); 
+    return false;  
   }
-  var C18 = 31 - (sum % 31);
-  if (C18 == 31) {
-      C18 = '0';
-  }else{
-      C18 = str.substr(C18,1);
+  
+  var sum = 0;
+  var arr=new Array();
+  arr=creditCode.split("");
+  for(var i=0;i<arr.length-1;i++){
+    var c=arr[i];
+    if(typeof(c)=="NaN"||typeof(c)=="undefined"){
+     toastr.warning('请输入正确的信用代码证!'); 
+      return false;
+    }
+    var a=getMapC(c);//获得代码对应数值
+    if(typeof(a)=="NaN"||typeof(a)=="undefined"){
+      toastr.warning('请输入正确的信用代码证!'); 
+      return false;
+    }
+    var b=getMapW((i+1).toString()); //获得当前位数权重
+    if(typeof(b)=="NaN"||typeof(b)=="undefined"){
+      toastr.warning('请输入正确的信用代码证!'); 
+      return false;
+    }
+    sum += a * b;
+    if(typeof(sum)=="NaN"||typeof(sum)=="undefined"){
+      toastr.warning('请输入正确的信用代码证!'); 
+      return false;
+    }
   }
 
-		if(creditCode.substr(17,1)== C18){ 
-		    toastr.info('校验通过!'); 
-			return true;     
-		}else{   
-			toastr.warning('校验不通过!'); 
-			return false;      
-		}       
-	}
+  var mod=sum%31;
+  var res = getMapR((31-mod).toString());//计算校验码
+  if(typeof(res)=="NaN"||typeof(res)=="undefined"){
+      toastr.warning('请输入正确的信用代码证!'); 
+      return false;
+  }
+  console.info(res.toString());
+  console.info(arr[arr.length-1]);
+  if(arr[arr.length-1]!=(res.toString())){
+    toastr.warning('请输入正确的信用代码证!'); 
+    return false;
+  }
+   toastr.info('校验通过!'); 
+  return true;
+}
+
+function getMapC(pa){
+  var result;
+  switch (pa) {
+     case "0" :
+          result=0;
+          break;
+       case "1" :
+          result=1;
+          break;
+       case "2" :
+          result=2;
+          break;
+       case "3" :
+          result=3;
+          break;
+       case "4" :
+          result=4;
+          break;
+       case "5" :
+          result=5;
+          break;
+       case "6" :
+          result=6;
+          break;
+       case "7" :
+          result=7;
+          break;
+       case "8" :
+          result=8;
+          break;
+       case "9" :
+          result=9;
+          break;
+       case "A" :
+          result=10;
+          break;
+       case "B" :
+          result=11;
+          break;
+       case "C" :
+          result=12;
+          break;
+       case "D" :
+          result=13;
+          break;
+       case "E" :
+          result=14;
+          break;
+       case "F" :
+          result=15;
+          break;
+       case "G" :
+          result=16;
+          break;
+       case "H" :
+          result=17;
+          break;
+       case "J" :
+          result=18;
+          break;
+       case "K" :
+          result=19;
+          break;
+       case "L" :
+          result=20;
+          break;
+       case "M" :
+          result=21;
+          break;
+       case "N" :
+          result=22;
+          break;
+       case "P" :
+          result=23;
+          break;
+       case "Q" :
+          result=24;
+          break;
+       case "R" :
+          result=25;
+          break;
+       case "T" :
+          result=26;
+          break;
+       case "U" :
+          result=27;
+          break;
+       case "W" :
+          result=28;
+          break;
+       case "X" :
+          result=29;
+          break;
+       case "Y" :
+          result=30;
+          break;
+  }
+  return result;
+}
+
+function getMapW(pa){
+  var result;
+  switch (pa) {
+     case "1" :
+          result=1;
+          break;
+       case "2" :
+          result=3;
+          break;
+       case "3" :
+          result=9;
+          break;
+       case "4" :
+          result=27;
+          break;
+       case "5" :
+          result=19;
+          break;
+       case "6" :
+          result=26;
+          break;
+       case "7" :
+          result=16;
+          break;
+       case "8" :
+          result=17;
+          break;
+       case "9" :
+          result=20;
+          break;
+       case "10" :
+          result=29;
+          break;
+       case "11" :
+          result=25;
+          break;
+       case "12" :
+          result=13;
+          break;
+       case "13" :
+          result=8;
+          break;
+       case "14" :
+          result=24;
+          break;
+       case "15" :
+          result=10;
+          break;
+       case "16" :
+          result=30;
+          break;
+       case "17" :
+          result=28;
+          break;
+  }
+  return result;
+}
+
+function getMapR(pa){
+  var result;
+  switch (pa) {
+     case "0" :
+          result="0";
+          break;
+       case "1" :
+          result="1";
+          break;
+       case "2" :
+          result="2";
+          break;
+       case "3" :
+          result="3";
+          break;
+       case "4" :
+          result="4";
+          break;
+       case "5" :
+          result="5";
+          break;
+       case "6" :
+          result="6";
+          break;
+       case "7" :
+          result="7";
+          break;
+       case "8" :
+          result="8";
+          break;
+       case "9" :
+          result="9";
+          break;
+       case "10" :
+          result="A";
+          break;
+       case "11" :
+          result="B";
+          break;
+       case "12" :
+          result="C";
+          break;
+       case "13" :
+          result="D";
+          break;
+       case "14" :
+          result="E";
+          break;
+       case "15" :
+          result="F";
+          break;
+       case "16" :
+          result="G";
+          break;
+       case "17" :
+          result="H";
+          break;
+       case "18" :
+          result="J";
+          break;
+       case "19" :
+          result="K";
+          break;
+       case "20" :
+          result="L";
+          break;
+       case "21" :
+          result="M";
+          break;
+       case "22" :
+          result="N";
+          break;
+       case "23" :
+          result="P";
+          break;
+       case "24" :
+          result="Q";
+          break;
+       case "25" :
+          result="R";
+          break;
+       case "26" :
+          result="T";
+          break;
+       case "27" :
+          result="U";
+          break;
+       case "28" :
+          result="W";
+          break;
+       case "29" :
+          result="X";
+          break;
+       case "30" :
+          result="Y";
+          break;
+       case "31" :
+          result="0";
+          break;
+  }
+  return result;
+}
+
 
 /*统一社会信用代码生成*/
 function generatorCreditCode(){
   $("#creditCode").val("");   
 
-  var regOrg = getRegistrationOrg("9");//默认工商
+  var regOrg = getRegOrgCode("9");//默认工商
   var orgtype = "1";//默认企业
   var num = parseInt(Math.random()*99);
   var area = areas[num];
@@ -61,29 +341,17 @@ function generatorCreditCode(){
   var wi =[1,3,9,27,19,26,16,17,20,29,25,13,8,24,10,30,28];
   var sum = 0;
     for (var i = 0; i < 17; i++) {
-      sum += str.indexOf(code.charAt(i)) * wi[i];
+      sum += getMapC(code.charAt(i))* getMapW((i+1).toString());
   }
   var C18 = 31 - (sum % 31);
   if (C18 == 31) {
       C18 = '0';
-  }else{
-      C18 = str.substr(C18,1);
-    }
-   $("#creditCode").val(code+C18);   
+  }
+   $("#creditCode").val(code+getMapR(C18.toString()));   
 }
-
-
-/**  
-* 返回一个随机的大写字母
-*/  
-function getUpperCharacter(){  
-	character = String.fromCharCode(Math.floor( Math.random() * 26) + "A".charCodeAt(0)); 
-	return character; 
-}
-
 
 /*获得登记机构*/
-function getRegistrationOrg(num){
+function getRegOrgCode(num){
 	var result;
 	switch(num){
 		case "1":
@@ -112,7 +380,7 @@ function getRegistrationOrg(num){
           break; 
         case "9":
           result=9; //工商
-          break; 
+          break;
         case "10":
           result=A; //旅游局
           break;
